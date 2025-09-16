3c2bb59c08383f7884a518438c237593b7c539b5
jrecka <jolantua@gmail.com>
2025-09-16 15:14:47 +0200

    Display product details dynamically using router id

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    @for (title of productTitles; track title) {
+      <ul>
+        <li><a [routerLink]="['details', $index]">{{ title }} | {{ $index }}</a></li>
+      </ul>
+
+    }
+    <router-outlet/>
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
--- projects/08-routing-recap/src/app/details/details.component.ts
+++ projects/08-routing-recap/src/app/details/details.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-details',
@@ -7,16 +7,22 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <section>
-      <p>Title</p>
+      <p>{{ this.productList[productId].title }}</p>
       <ul>
-        <li>Price</li>
-        <li>Description</li>
+        <li>{{ this.productList[productId].price }}</li>
+        <li>{{ this.productList[productId].description }}</li>
       </ul>
     </section>
   `,
   styles: ``,
 })
 export class DetailsComponent {
+  productId = 0;
+
+  @Input() set id(value: number) {
+    this.productId = value;
+  }
+
   productList = [
     {
       title: 'Product 1',
